1a285e744df7d5b9fd2d45268cdf5a37fa1c6fe3
Pavel Nikolov <pavel.nikolov@adamos.com>
2021-04-29 12:45:11 +0200

    Bug fixing

--- client/index.js
+++ client/index.js
@@ -4,20 +4,18 @@ const MDM = require('../lib/controllers/mdm')
 const Permission = require('../lib/controllers/permission')
 
 class Client {
-    constructor({access_token, environment?, baseURL?}) {
+    constructor({access_token, environment, baseURL}) {
         if (!access_token) {
             throw new Error(`Missing parameter 'access_token'!`)
         }
         http.setEnvironment(environment)
         http.setAuthHeader(access_token)
 
-        http.defaults.baseURL = baseURL
-
         this.mdm = new MDM()
         this.permission = new Permission()
     }
 
-    static async build({client_id, client_secret, environment?, baseURL?}) {
+    static async build({client_id, client_secret, environment, baseURL}) {
         if (!client_id) {
             throw new Error(`Missing parameter 'client_id'!`)
         }
